672b4c6238a033b012d67d098030d6ff9a2cf961
Muneera Bin Hotan <muneerabinhotan@gmail.com>
2019-11-06 15:57:20 +0300

    +

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -29,26 +29,26 @@ class HomePage extends Component{
             <Carousel>
                 <Carousel.Item>
                     <img className="d-block w-75 mx-auto"src={this.props.posts[0].urls.regular} alt="First slide"/>
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
 
                 <Carousel.Item>
                     <img className="d-block w-75 mx-auto"src={this.props.posts[1].urls.regular} alt="First slide"/>
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
 
                 <Carousel.Item>
                     <img className="d-block w-75 mx-auto"src={this.props.posts[2].urls.regular} alt="First slide"/>
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
             </Carousel>
             )}
